75ce23ee412d75ab489251622fb51ce9fff71edd
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-11 15:06:41 +0200

    Add formatFullname tests to formatted arg

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,14 +1,17 @@
 module.exports = (fullName) => {
-  if (fullName !== 'string') return 'Error';
+  if (typeof fullName !== 'string') return 'Error';
+
+  let spaces = fullName.split(' ').length - 1;
+  if (spaces !== 1) return 'Error';
 
   const [firstName, lastName] = fullName.split(' ');
-
   if (!firstName || !lastName) return false;
 
   return (
-    firstName[0].toUpperCase +
+    firstName[0].toUpperCase() +
     firstName.slice(1).toLowerCase() +
-    lastName[0].toUpperCase +
+    ' ' +
+    lastName[0].toUpperCase() +
     lastName.slice(1).toLowerCase()
   );
 };
--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -2,11 +2,22 @@ const formatFullname = require('../formatFullname');
 const expect = require('chai').expect;
 
 describe('formatFullname', () => {
-  it('', () => {
+  it('should return an error if "fullName" arg is not a string', () => {
     expect(formatFullname(undefined)).to.equal('Error');
     expect(formatFullname(12)).to.equal('Error');
     expect(formatFullname({})).to.equal('Error');
     expect(formatFullname([])).to.equal('Error');
     expect(formatFullname(() => {})).to.equal('Error');
+  });
+  it('should return an error if "fullName" arg does not contains exactly one " "', () => {
+    expect(formatFullname('LoremIpsum')).to.equal('Error');
+    expect(formatFullname('Lorem Ipsum ')).to.equal('Error');
+    expect(formatFullname(' Lorem Ipsum')).to.equal('Error');
+    expect(formatFullname('Lorem Ipsum Dolor')).to.equal('Error');
+  });
+  it('should return properly formatted "fullName"" if proper args', () => {
+    expect(formatFullname('Lorem iPsUm')).to.equal('Lorem Ipsum');
+    expect(formatFullname('LOrEm Ipsum')).to.equal('Lorem Ipsum');
+    expect(formatFullname('LOrEm IpSuM')).to.equal('Lorem Ipsum');
   });
 });
